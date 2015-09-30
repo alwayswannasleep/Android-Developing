2484f117820e58756ae44c95f544609e7904c637
Vlad Kraevskiy <vladkraevskii@gmail.com>
2015-09-30 22:06:52 +0300

    Sync MyApp

--- MyApplication/app/src/main/java/com/kraevskiy/myapplication/app/MainActivity.java
+++ MyApplication/app/src/main/java/com/kraevskiy/myapplication/app/MainActivity.java
@@ -1,23 +1,27 @@
 package com.kraevskiy.myapplication.app;
 
 import android.app.Activity;
-import android.os.Environment;
 import android.os.Bundle;
 import android.util.Log;
-
-import java.io.File;
-
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-    public final String STORAGE_TAG = "Storage";
+    private final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        File storage = Environment.getDataDirectory();
-        Log.d(STORAGE_TAG, storage.getPath());
+        Button myButton = (Button) findViewById(R.id.button_1);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Click");
+            }
+        });
     }
 }
