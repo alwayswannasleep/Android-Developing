8b7c6241b985916520939d4ce9e4e331d792b4af
Vlad Kraevskiy <vladkraevskii@gmail.com>
2015-10-01 14:07:06 +0300

    Printing all files on Device in LOG.

--- MyApplication/app/src/main/java/com/kraevskiy/myapplication/app/MainActivity.java
+++ MyApplication/app/src/main/java/com/kraevskiy/myapplication/app/MainActivity.java
@@ -2,9 +2,12 @@ package com.kraevskiy.myapplication.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends Activity {
 
@@ -15,6 +18,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        File storage = Environment.getRootDirectory();
+
+        PrintFilePaths(storage);
+
         Button myButton = (Button) findViewById(R.id.button_1);
 
         myButton.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +30,24 @@ public class MainActivity extends Activity {
                 Log.d(TAG, "Click");
             }
         });
+    }
+
+    private void PrintFilePaths(File file) {
+        Log.d(TAG, file.getPath());
+
+        File[] files = file.listFiles();
+        if (files == null) {
+            return;
+        }
+
+        for (File f : files) {
+            if (f.isDirectory()){
+                Log.d(TAG, f.getPath());
+
+                PrintFilePaths(f);
+            } else {
+                Log.d(TAG, f.getPath());
+            }
+        }
     }
 }
